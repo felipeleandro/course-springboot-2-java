ba0e32d257b87d25c3400ef4d253d878bf901f61
Felipe Vedoveto <vedoveto99@gmail.com>
2020-02-09 10:53:38 -0300

    Exception handling - findById

--- src/main/java/com/vedoveto/course/services/UserService.java
+++ src/main/java/com/vedoveto/course/services/UserService.java
@@ -1,5 +1,6 @@
 package com.vedoveto.course.services;
 
+import com.vedoveto.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vedoveto.course.repositories.UserRepository;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert (User obj) {
